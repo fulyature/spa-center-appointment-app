74daa2a58196f64164712d3aec781fba817ae525
fulya_ture <fulyature@gmail.com>
2023-07-30 14:08:44 +0300

    initialized

---
+++ src/helper/data.jsx
@@ -0,0 +1,86 @@
+export const appointmentData = [
+  {
+    id: 1,
+    customer: "Selana Vermont",
+    day: new Date(),
+    consulted: true,
+    proficient: "Tarık Valery",
+  },
+  {
+    id: 2,
+    customer: "Jacob Coming",
+    day: new Date(),
+    consulted: false,
+    proficient: "Jessica White",
+  },
+  {
+    id: 3,
+    customer: "Thomas Black",
+    day: new Date(),
+    consulted: true,
+    proficient: "Hormaie Carpanter",
+  },
+];
+
+export const doctorData = [
+  {
+    id: 1,
+    name: "Hazel Valery",
+    service: "Swedish Massage",
+    img: "./img/dr1.jpg",
+  },
+  {
+    id: 2,
+    name: "Hormaie Carpanter",
+    service: "Aromatherapy Massage",
+    img: "./img/dr2.jpg",
+  },
+  {
+    id: 3,
+    name: "Arianna Mary",
+    service: "Deep Tissue Massage",
+    img: "./img/dr3.jpg",
+  },
+  {
+    id: 4,
+    name: "Jessica White",
+    service: "Reflexology",
+    img: "./img/dr4.jpg",
+  },
+  {
+    id: 5,
+    name: "Harry Coming",
+    service: "Medical Massage",
+    img: "./img/dr9.jpg",
+  },
+  {
+    id: 6,
+    name: "Kristensen Connor",
+    service: "Hot Stone Therapy",
+    img: "./img/dr6.jpg",
+  },
+  {
+    id: 7,
+    name: "Oliver Graham",
+    service: "Thai Massage",
+    img: "./img/dr7.jpg",
+  },
+  {
+    id: 8,
+    name: "Mary Motley",
+    service: "Indian Head Massage",
+    img: "./img/dr8.jpg",
+  },
+  {
+    id: 9,
+    name: "Kate A",
+    service: "Lomi Lomi Massage",
+    img: "./img/dr7.jpg",
+  },
+  {
+    id: 10,
+    name: "Mary Motley",
+    service: "Back, Shoulder and Neck Massage",
+    img: "./img/dr8.jpg",
+  },
+];
