6d35a193822ac9df2991648fb5ae43157c4e3a9a
fulya_ture <fulyature@gmail.com>
2023-07-31 14:28:45 +0300

    created Modal

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -1,5 +1,37 @@
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+
 function AddModal() {
-  return <div className="">Modal</div>;
+  return (
+    <>
+      <Modal>
+        <Modal.Header closeButton>
+          <Modal.Title></Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="name">
+              <Form.Label>Patient Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter name" required />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="date">
+              <Form.Label>Date</Form.Label>
+              <Form.Control type="date" placeholder="Date" required />
+            </Form.Group>
+
+            <div className="text-center">
+              <Button variant="success" type="submit" className="me-2">
+                Save
+              </Button>
+              <Button variant="danger">Close</Button>
+            </div>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
 }
 
 export default AddModal;
--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -3,20 +3,31 @@ import Container from "react-bootstrap/Container";
 import { serviceData } from "../helper/data";
 import { Row, Col } from "react-bootstrap";
 
+import { useState } from "react";
+
 const Service = () => {
-  console.log(serviceData);
+  const [show, setShow] = useState(false);
+
+  const handleImgClick = () => {
+    setShow(true);
+  };
   return (
     <Container className="p-2">
-      <h3 className="display-6 mb-" style={{ color: "#7a7672" }}>
+      <h3 className="display-6 mb-3" style={{ color: "#7a7672" }}>
         OUR SERVICES
       </h3>
 
-      <Row>
+      <Row className="flex-wrap">
         {serviceData.map((item) => {
           console.log(item, "item");
           return (
             <Col xs={6} sm={4} md={3}>
-              <img src={item.img} alt={item.name} className="img-thumbnail" />
+              <img
+                src={item.img}
+                alt={item.name}
+                className="service-img"
+                onClick={handleImgClick}
+              />
               <h5>{item.name}</h5>
               <h6>{item.service}</h6>
             </Col>
@@ -24,7 +35,7 @@ const Service = () => {
         })}
       </Row>
 
-      <AddModal />
+      <AddModal show={show} handleClose={() => setShow(false)} />
     </Container>
   );
 };
